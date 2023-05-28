e871dc7c43a303aad4fb666b1b1838b7327e598a
Amresh Tripathi <amresh2k@gmail.com>
2023-05-28 19:25:22 +0530

    Added signature

--- DBMS_AMRESH_Project4.sql
+++ DBMS_AMRESH_Project4.sql
@@ -1,5 +1,8 @@
---
--- Table structure for table `category`
+-- * @author          Amresh Tripathi 
+-- * @date            25-Apr-2023
+-- * @version         1.0
+-- * @DESC            CREATE ALL TABLES and INSERT DATA
+-- Table structure for table `category`  by Amresh
 --
 
 CREATE TABLE IF NOT EXISTS `category` (
@@ -65,27 +68,27 @@ CREATE TABLE IF NOT EXISTS `order` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `orders`
+-- Dumping data for table `order`  by Amresh
 --
 
-INSERT INTO `orders` (`ord_id`, `ord_date`, `cus_id`, `pricing_id`) VALUES
-(101, '2021-10-06', 2, 1),
-(102, '2021-10-12', 3, 5),
-(103, '2021-09-16', 5, 2),
-(104, '2021-10-05', 1, 1),
-(105, '2021-08-16', 4, 3),
-(106, '2021-08-18', 1, 9),
-(107, '2021-09-01', 3, 7),
-(108, '2021-09-07', 5, 6),
-(109, '2021-09-10', 5, 3),
-(110, '2021-09-10', 2, 4),
-(111, '2021-09-15', 4, 5),
-(112, '2021-09-16', 4, 7),
-(113, '2021-09-16', 1, 8),
-(114, '2021-09-16', 3, 5),
-(115, '2021-09-16', 5, 3),
-(116, '2021-09-17', 2, 14),
-(117, '2023-05-02', 4, 3);
+INSERT INTO `order` (`ord_id`, `ord_date`, `cus_id`, `pricing_id`, `ord_amount`) VALUES
+(101, '2021-10-06', 2, 1, 1500),
+(102, '2021-10-12', 3, 5, 1000),
+(103, '2021-09-16', 5, 2, 30000),
+(104, '2021-10-05', 1, 1, 1500),
+(105, '2021-08-16', 4, 3, 3000),
+(106, '2021-08-18', 1, 9, 1450),
+(107, '2021-09-01', 3, 7, 789),
+(108, '2021-09-07', 5, 6, 780),
+(109, '2021-09-10', 5, 3, 3000),
+(110, '2021-09-10', 2, 4, 2500),
+(111, '2021-09-15', 4, 5, 1000),
+(112, '2021-09-16', 4, 7, 789),
+(113, '2021-09-16', 1, 8, 31000),
+(114, '2021-09-16', 3, 5, 1000),
+(115, '2021-09-16', 5, 3,3000),
+(116, '2021-09-17', 2, 14,99);
+
 
 -- --------------------------------------------------------
 
@@ -103,7 +106,7 @@ CREATE TABLE IF NOT EXISTS `product` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `product`
+-- Dumping data for table `product` by Amresh
 --
 
 INSERT INTO `product` (`pro_id`, `pro_name`, `pro_desc`, `cat_id`) VALUES
@@ -134,27 +137,7 @@ CREATE TABLE IF NOT EXISTS `rating` (
   KEY `ord_id` (`ord_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `rating`
---
 
-INSERT INTO `rating` (`rat_id`, `ord_id`, `stars`) VALUES
-(1, 101, 4),
-(2, 102, 3),
-(3, 103, 1),
-(4, 104, 2),
-(5, 105, 4),
-(6, 106, 3),
-(7, 107, 4),
-(8, 108, 4),
-(9, 109, 3),
-(10, 110, 5),
-(11, 111, 3),
-(12, 112, 4),
-(13, 113, 2),
-(14, 114, 1),
-(15, 115, 1),
-(16, 116, 0);
 
 -- --------------------------------------------------------
 
@@ -171,8 +154,7 @@ CREATE TABLE IF NOT EXISTS `supplier` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `supplier`
---
+-- Dumping data for table `supplier` by Amresh
 
 INSERT INTO `supplier` (`supp_id`, `supp_name`, `supp_city`, `supp_phone`) VALUES
 (1, 'Rajesh Retails', 'Delhi', '1234567890'),
@@ -184,7 +166,7 @@ INSERT INTO `supplier` (`supp_id`, `supp_name`, `supp_city`, `supp_phone`) VALUE
 -- --------------------------------------------------------
 
 --
--- Table structure for table `supplier_pricing`
+-- Table structure for table `supplier_pricing` by Amresh
 --
 
 CREATE TABLE IF NOT EXISTS `supplier_pricing` (
@@ -198,7 +180,7 @@ CREATE TABLE IF NOT EXISTS `supplier_pricing` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `supplier_pricing`
+-- Dumping data for table `supplier_pricing` by Amresh
 --
 
 INSERT INTO `supplier_pricing` (`pricing_id`, `pro_id`, `supp_id`, `supp_price`) VALUES
@@ -218,3 +200,27 @@ INSERT INTO `supplier_pricing` (`pricing_id`, `pro_id`, `supp_id`, `supp_price`)
 (14, 6, 1, 99),
 (15, 2, 5, 2999),
 (16, 5, 2, 2999);
+
+-- --------------------------------------------------------
+
+--
+-- Dumping data for table `rating` by Amresh
+--
+
+INSERT INTO `rating` (`rat_id`, `ord_id`, `stars`) VALUES
+(1, 101, 4),
+(2, 102, 3),
+(3, 103, 1),
+(4, 104, 2),
+(5, 105, 4),
+(6, 106, 3),
+(7, 107, 4),
+(8, 108, 4),
+(9, 109, 3),
+(10, 110, 5),
+(11, 111, 3),
+(12, 112, 4),
+(13, 113, 2),
+(14, 114, 1),
+(15, 115, 1),
+(16, 116, 0);
